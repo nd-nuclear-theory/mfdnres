9b5e0655ae965e31f068a2a0e455e2b9e283218a
Patrick Fasano <patrick@patrickfasano.com>
2017-02-07 10:35:06 -0500

    filename: Add filename format 7 parser

--- filename_formats/format_7_ho.py
+++ filename_formats/format_7_ho.py
@@ -1,11 +1,12 @@
-""" format_6_ho.py -- declares descriptor parser
+""" format_7.py -- declares descriptor parser
 
     Language: Python 3
     Mark A. Caprio
+    Patrick J. Fasano
     University of Notre Dame
 
-    7/3/15 (mac): Initiated (based on format_5_ho.py).
-    Last modified 7/6/15.
+    2/6/17 (pjf): Initiated (based on format_6_ho.py).
+    Last modified 2/6/17.
 
 """
 
@@ -35,16 +36,19 @@ def parser(filename):
         r"run(?P<run>\w+)"
         r"\-mfdn"
         r"\-(?P<descriptor>"
-        # descriptor conents
+        # descriptor contents
         r"Z(?P<Z>\d+)\-N(?P<N>\d+)"
-        r"\-(?P<interaction>[^\-]+)\-(?P<coulomb>\d)"
+        r"\-(?P<interaction>[^\-]+)\-coul(?P<coulomb>\d)"
         r"\-hw(?P<hw>[\d\.]+)"
-        r"\-aL(?P<lawson>[\d\.]+)"
-        r"\-Nmax(?P<Nmax>\d+)(?P<mixed_parity_flag>x?)(?P<fci_flag>\-fci)?"
+        r"\-a_cm(?P<lawson>[\d\.]+)"
+        r"\-Nmax(?P<Nmax>\d+)(?P<mixed_parity_flag>x)?(?P<fci_flag>\-fci)?"
         r"\-Mj(?P<Mj>[\d\.]+)"
         r"\-lan(?P<lanczos>\d+)"
-        # epilog
-        r").res"
+        r"\-tol(?P<tolerance>\d+\.\d+[eE][+-]\d+)"
+        r"((?P<natural_orbital_flag>\-natorb)\-no(?P<natural_orbital_iteration>\d+))?"
+        # # epilog
+        # r").res"
+        r")"
     )
 
     conversions = {
@@ -57,7 +61,9 @@ def parser(filename):
         "mixed_parity_flag" : (lambda s  :  (s=="x")),
         "fci_flag" : (lambda s  :  (s=="-fci")),
         "Mj" : float,
-        "lanczos" : int
+        "lanczos" : int,
+        "natural_orbital_flag" : (lambda s  :  (s=="-natorb")),
+        "natural_orbital_iteration" : (lambda i  :  int(i) if (i is not None) else None)
         }
 
     match = regex.match(filename)
@@ -72,11 +78,16 @@ def parser(filename):
 
     return info
 
-mfdnres.descriptor.register_filename_format("format_6_ho",parser)
+mfdnres.descriptor.register_filename_format("format_7",parser)
 
 if (__name__ == "__main__"):
 
-    filename = r"run0376-mfdn-Z4-N6-JISP16-1-hw15.000-aL100-Nmax04-Mj0.0-lan1500.res"
+    filename = r"run0000-mfdn-Z2-N6-Daejeon16-coul1-hw05.000-a_cm20-Nmax02-Mj0.0-lan500-tol1.0e-06-natorb-no0.res"
+    info = parser(filename)
+    print(filename)
+    print(info)
+
+    filename = r"run0000-mfdn-Z2-N6-Daejeon16-coul1-hw05.000-a_cm20-Nmax02x-Mj0.0-lan500-tol1.0e-06.res"
     info = parser(filename)
     print(filename)
     print(info)
