7356853793081f2a448cf7bdbd2933f0f64061e1
Julie Lynn Butler <jbutler7@crcfe01.crc.nd.edu>
2017-06-22 21:39:48 -0400

    res_parser: spncci results file parser
    Currently only works with an unfinished results
    file. Graphs energy vs hw.
    
    Currently only takes in an unfinished results file
    and makes a table and graph of energy versus hw.

---
+++ res_formats/spncciParser.py
@@ -0,0 +1,117 @@
+# Imports the parser from the make_dict file
+from make_dict import make_dict_spncii
+
+# Imported to allow for graphing
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+
+"""
+    spncii Parser Revision 1
+    Julie Butler
+    June 21, 2017
+    Python3
+
+    The controlling file for the spncci results file
+    parser.  The parser used here is the method
+    make_dict_spncci, stored in the file make_dict.py
+    (it is imported below).  Currently this file is set up
+    to parse the file whos name is provided below and make
+    a graph and table of energy versus hw.  This can be
+    expanded later to include more analysis.
+
+    Once the format of the results file is on a more
+    finalized form, this should be rearranged into a
+    class structure for ease of access to variables.
+"""
+
+
+def spnciiParser(fileName):
+    """
+        Arguments:
+            fileName: a string.  The filename of the results
+            file that needs to be analyzed.
+        Returned:
+            results_dict: a dictionary.  What is returned
+            from make_dic_spncci.
+
+        This method currently takes in a file names and
+        converts the file contents into a list of strings,
+        where each string is a line from the file.  It then
+        takes the list and passes it to the parser
+        make_dict_spncci.  The dictionary returned from the
+        parser is then printed and returned by the method.
+        This method will be expanded and possibly incoporated
+        into a class structure when the format of the results
+        files is finalized.
+    """
+    # Stores the results from inputting the file
+    results = []
+
+    # Inputs the file passed as an argument and stores the
+    # parsed file contents in results
+    with open(fileName, 'rt') as fin:
+        for row in fin:
+            results.append(row)
+
+    # Passes results to the parser make_dict_spncci and
+    # stores the returned dictionary and list
+    results_dict, order = make_dict_spncii(results)
+
+    # Returns the dictionary
+    return results_dict
+
+
+res = spnciiParser('runmac0405-Z3-N3-Nsigmamax02-Nmax02-spncii.res')
+# This section of code makes the graph of energy versus h bar omega.
+# It also makes a table of the form nsigmamax,nmax,hw,energy for each
+# hw value and writes those to a file
+# The list of hw values used in the file are contained in the hw
+# variable of the Mesh section of the results file.  This line
+# retrieves those values as a list
+hws = res['Mesh']['hw']
+hws = [float(i) for i in hws]
+
+# Retrieves the energies based on the hw value (which is the key)
+# and stores them in the list energies.
+energies = []
+for x in hws:
+    energies.append(res[x][0][1])
+
+# Retrieves the value of the variables nmax and nsigmamax, both contained
+# in the section 'Space'
+nmax =  res['Space']['Nmax']
+nsigmamax = res['Space']['Nsigmamax']
+
+# The names for the outputted graph and table
+graphName = 'spncciTest.png'
+outputFileName = 'spncciTest.txt'
+
+# The lable for the graph
+legendLabel = 'Nsigmamax = ' + str(nsigmamax) + '; Nmax = ' + str(nmax)
+
+# This section creates the graph and table and saves them as the file names
+# specified above, if there are the same number of elements in hws and energies
+if len(hws) == len(energies):
+    # Formats the data to be written to the file and then writes the data to the
+    # file with a lable, denoted by the hashtag.  This label can be removed if
+    # needed.
+    tofile = []
+    for i in range(len(hws)):
+        temp = str(nsigmamax)  + ',' + str(nmax) + ',' + str(hws[i]) + ',' + str(energies[i]) + '\n'
+        tofile.append(temp)
+    with open(outputFileName, 'w') as fout:
+        fout.write('#nsigmamax,nmax,hw,Energy\n')
+        for x in tofile:
+            fout.write(x)
+
+    # This section created the graph and saves it to the file name specified
+    # above.  To display the graph instead of saving it, change the last line
+    # to 'plt.plot()' (no quotes).
+    plt.figure()
+    plt.plot (hws, energies, 'r-', label=legendLabel)
+    plt.xlabel('h-bar omega (MeV)')
+    plt.ylabel('Ground State Energy (Mev)')
+    plt.legend(bbox_to_anchor=(1, 1))
+    plt.savefig(graphName, bbox_inches='tight')
