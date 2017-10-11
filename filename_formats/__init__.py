9d3326efb4594c8bca71ee9c65fcbb6d5cd2a69a
Mark A. Caprio <mcaprio-nd-edu>
2017-10-10 21:45:14 -0400

    Interim renaming of mfdn file formats

--- filename_formats/__init__.py
+++ filename_formats/__init__.py
@@ -1,8 +1,8 @@
 __all__ = [
-    "format_5_ho",  # mac format 5, restricted to ho
-    "format_6_ho",  # mac format 6, restricted to ho
-    "format_7_ho",  # mac/pjf format 7, restricted to ho
-    "format_pm"     # pm typical format
+    "format_mfdn_5_ho",  # mac format 5, restricted to ho
+    "format_mfdn_6_ho",  # mac format 6, restricted to ho
+    "format_mfdn_7_ho",  # mac/pjf format 7, restricted to ho
+    "format_mfdn_pm"     # pm typical format
 ]
 
 # force registration of all parsers listed in __all__
