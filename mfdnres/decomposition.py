e924976a54ddbad57afbaa373ddaeacfc6de8dea
Mark A. Caprio <mcaprio-nd-edu>
2020-02-20 23:18:01 -0500

    decomposition: Provide eigenvalue input and overhaul to handle degenerate labels

--- mfdnres/decomposition.py
+++ mfdnres/decomposition.py
@@ -5,11 +5,14 @@ Patrick J. Fasano
 University of Notre Dame
 
 + 10/24/19 (pjf): Created, migrated from diagonalize_alphabeta.py.
++ 02/20/20 (mac): Provide eigenvalue input and overhaul to handle degenerate labels.
 """
 
 import numpy as np
 import scipy.linalg as linalg
 from . import histogram
+
+import mcscript.utils  # for value_range
 
 def read_lanczos(filename="mfdn_alphabeta.dat", **kwargs):
     """Read and parse mfdn_alphabeta file into d and e arrays for eigh_tridiagonal
@@ -23,9 +26,11 @@ def read_lanczos(filename="mfdn_alphabeta.dat", **kwargs):
     alphabeta = np.loadtxt(filename, usecols=(1, 2))
     return (alphabeta[:, 0], alphabeta[:-1, 1])
 
-
-def generate_decomposition(labels, expected_eigenvalues, alphabeta, **kwargs):
+def generate_decomposition_LEGACY(labels, expected_eigenvalues, alphabeta, **kwargs):
     """Generate decomposition from Lanczos alpha-beta matrix and expected eigenvalues.
+
+    This is the initial implementation which returns "low level" decomposition,
+    still by eigenvalues, as well as histogram.BinMapping object.
 
     Arguments:
         labels (list): irrep labels
@@ -54,3 +59,127 @@ def generate_decomposition(labels, expected_eigenvalues, alphabeta, **kwargs):
         binned_decomposition[eigval] += probability
         
     return (raw_decomposition,binned_decomposition)
+
+def generate_decomposition(alphabeta,eigenvalue_label_dict,verbose=False):
+    """Generate decomposition from Lanczos alpha-beta matrix and expected eigenvalues.
+
+    Arguments:
+        alphabeta (tuple): (alpha,beta)
+            alpha (np.array of float): alpha matrix elements
+            beta (np.array of float): beta matrix elements
+        eigenvalue_label_dict (dict): mapping from eigenvalue to list of labels
+
+    Returns:
+        decomposition (dict): probabilities binned by labels (as lists of degenerate labels)
+
+    """
+
+    # generate Lanczos decomposition
+    alpha, beta = alphabeta
+    eigvals, eigvecs = linalg.eigh_tridiagonal(alpha, beta)
+    raw_decomposition = [
+        (eigval,eigvecs[0, i]**2)
+        for i, eigval in enumerate(eigvals)
+    ]
+    
+    # bin Lanczos decomposition
+    expected_eigenvalues = sorted(eigenvalue_label_dict.keys())
+    label_groups = []
+    for eigenvalue in expected_eigenvalues:
+        label_groups.append(eigenvalue_label_dict[eigenvalue])
+        
+    bins = histogram.BinMapping.create_bisection_bins(expected_eigenvalues)
+    binned_decomposition = histogram.BinMapping(keys=label_groups, bins=bins)
+    for eigval, probability in raw_decomposition:
+        binned_decomposition[eigval] += probability
+        
+    # convert to dict for well-behaved access using label (rather than eigenvalue) as key
+    decomposition = binned_decomposition.as_dict()
+        
+    return decomposition
+
+def eigenvalue_label_dict_am(am_max,verbose=False):
+    """Generate eigenvalue dictionary for decomposition by angular momentum Casimir eigenvalue.
+
+    Arguments:
+        am_max (float): maximum expected angular momentum (integer or half-integer)
+
+    Returns:
+        (dict): mapping from eigenvalue to J (as float)
+
+    """
+
+    eigenvalue_label_dict = {
+        float(J*(J+1)): float(J)
+        for J in mcscript.utils.value_range(am_max%1,am_max,1)
+        }
+    return eigenvalue_label_dict
+
+def eigenvalue_label_dict_Nex(Nmax,verbose=False):
+    """Generate eigenvalue dictionary for decomposition by Nex.
+
+    Arguments:
+        Nmax (int): Nmax for calculation
+
+    Returns:
+        (dict): mapping from eigenvalue to Nex (as int)
+
+    """
+
+    print(mcscript.utils.value_range(Nmax%2,Nmax,2))
+    eigenvalue_label_dict = {
+        float(Nex): Nex
+        for Nex in mcscript.utils.value_range(Nmax%2,Nmax,2)
+        }
+    return eigenvalue_label_dict
+
+def read_eigenvalues(filename,verbose=False):
+    """Read table mapping irrep labels to Casimir eigenvalues.
+
+    Eigenvalue degeneracies are allowed.
+
+    File format:
+        label1 label2 ... eigenvalue
+
+    Arguments:
+        filename (str): Input filename
+
+    Returns:
+        (dict): Mapping from eigenvalue to list of degenerate labels
+    """
+
+    # read data
+    table = np.loadtxt(filename)
+    label_length = table.shape[1]-1  # all but last column constitutes label
+    if (verbose):
+        print(table)
+        
+    # collect labels by eigenvalue
+    eigenvalue_label_dict = {}
+    for row in table:
+        label = tuple(row[:-1])
+        eigenvalue = row[-1]
+        eigenvalue_label_dict.setdefault(eigenvalue,[])
+        eigenvalue_label_dict[eigenvalue].append(label)
+
+    # convert list of labels to tuples
+    #
+    # This is necessary so that they can be used as an immutable binning key.
+    for eigenvalue in eigenvalue_label_dict:
+        eigenvalue_label_dict[eigenvalue] = tuple(eigenvalue_label_dict[eigenvalue])
+    
+    # diagnostic output
+    if (verbose):
+        for (eigenvalue,label_list) in eigenvalue_label_dict.items():
+            print("{:12e} {:1d} {}".format(eigenvalue,len(label_list),label_list))
+
+    return eigenvalue_label_dict
+
+def print_decomposition(decomposition,label_format="s",probability_format="8.6f"):
+    """
+    """
+
+    format_str = "{{:{}}} {{:{}}}".format(label_format,probability_format)
+    for labels in sorted(decomposition.keys()):
+        print(format_str.format(labels,decomposition[labels]))
+    
