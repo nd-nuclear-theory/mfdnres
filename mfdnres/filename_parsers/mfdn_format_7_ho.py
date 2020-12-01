b8172f048944aadef2eaaa4f7bfcecebe613466d
Patrick Fasano <patrick@patrickfasano.com>
2020-12-01 13:20:35 -0600

    filename_parsers: Support natorb base state info in descriptor

--- mfdnres/filename_parsers/mfdn_format_7_ho.py
+++ mfdnres/filename_parsers/mfdn_format_7_ho.py
@@ -14,7 +14,9 @@
     12/17/19 (mac): Support format_7_trans (i.e., make diagonalization-related fields optional).
     06/12/20 (mac): Support optional subset index from format_7_trans.
     09/17/20 (mac): Support code names for postprocessor output ("transitions-{ob,tb}").
-
+    12/01/20 (pjf):
+        + Support natural orbital base state information in descriptor.
+        + Support "decomp" flag.
 """
 
 import re
@@ -50,10 +52,15 @@ def parser(filename):
         r"(\-Mj(?P<M>-?[\d\.]+))?"
         r"(\-lan(?P<lanczos>\d+))?"
         r"(\-tol(?P<tolerance>\d+\.\d+[eE][+-]\d+))?"
-        r"((?P<natural_orbital_flag>\-natorb)?\-no(?P<natural_orbital_iteration>\d+))?"
+        r"("   # begin natorb group
+          r"(?P<natural_orbital_flag>\-natorb)"
+          r"(\-J(?P<J>[\d\.]+)\-g(?P<g>[01])\-n(?P<n>[\d]+))?"
+          r"(\-no(?P<natural_orbital_iteration>\d+))?"
+        r")?"  # end natorb group
+        r"(?P<decomposition_flag>\-decomp)?"
         r"(\-subset(?P<subset_index>\d+))?"
         # epilog
-        r").res"
+        r").(?P<extension>((res)|(out)|(lanczos)))"
     )
 
     conversions = {
@@ -70,6 +77,8 @@ def parser(filename):
         "M" : float,
         "lanczos" : int,
         "natural_orbital_flag" : (lambda s  :  (s=="-natorb")),
+        "J": float, "g": int, "n": int,
+        "decomposition_flag" : (lambda s  :  (s=="-decomp")),
         "natural_orbital_iteration" : int
         }
 
@@ -82,6 +91,9 @@ def parser(filename):
     for key in conversions:
         conversion = conversions[key]
         info[key] = conversion(info[key]) if (info[key] is not None) else None
+
+    # build natorb base state
+    info["natorb_base_state"] = (info.pop("J"), info.pop("g"), info.pop("n"))
 
     return info
 
@@ -100,6 +112,11 @@ if (__name__ == "__main__"):
     print(info)
 
     filename = r"runpjf0015-mfdn15-Z3-N4-JISP16-coul1-hw20.000-a_cm40-Nmax02-Mj0.5-lan1000-tol1.0e-06-natorb-no0.res"
+    info = input.parse_filename(filename, filename_format="mfdn_format_7_ho")
+    print(filename)
+    print(info)
+
+    filename = r"runpjf0069-mfdn15-Z2-N1-Daejeon16-coul1-hw22.500-a_cm0-Nmax14-Mj0.5-lan400-tol1.0e-06-natorb-J00.5-g0-n01-no0.res"
     info = input.parse_filename(filename, filename_format="mfdn_format_7_ho")
     print(filename)
     print(info)
