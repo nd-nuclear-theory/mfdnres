048fbfd7dbbf7eab69db0bee42334dfe425f33ff
Patrick Fasano <patrick@patrickfasano.com>
2022-04-21 13:07:49 -0400

    setup: Add more-itertools dependency

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     packages=find_packages(include='mfdnres*'),
     install_requires=[
         "numpy>=1.0.0",
+        "more-itertools>=2.4",
     ],
     extras_require={
         'analysis': ['pandas>=1.2.0', 'matplotlib>=3.4.1', 'more_itertools']
