36b619dd98a7bcaff251bd8da2bbb71a050a6755
zzhou6 <zzhou6@nd.edu>
2024-01-16 21:31:28 -0500

    filename_parsers: Add support for menj

--- mfdnres/filename_parsers/__init__.py
+++ mfdnres/filename_parsers/__init__.py
@@ -5,6 +5,7 @@ __all__ = (
     "mfdn_format_8",     # pjf/pm format 8, general truncation
     "mfdn_format_c1",    # pjf counting format 1
     "mfdn_format_pm",    # pm typical format
+    "mfdn_format_menj",  # menj format
     "spncci",            # basic spncci format, with truncation left as string
 )
 
