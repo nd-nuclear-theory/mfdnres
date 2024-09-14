0d3262efdce0b1e197205b9d68d38f749ba12443
Mark A. Caprio <mcaprio-nd-edu>
2024-09-14 18:30:06 -0400

    observable_differencing: Add ExcitationEnergyExponentialExtrapolationSeparate observable

--- mfdnres/observable_differencing.py
+++ mfdnres/observable_differencing.py
@@ -7,6 +7,7 @@
     - 07/08/23 (mac): Add RelativeDifference observable.
     - 03/04/24 (mac): Add ExponentialExtrapolation observable.
     - 03/21/24 (mac): Extract Nmax_shifted() utility to observable.py.
+    - 09/14/24 (mac): Add ExcitationEnergyExponentialExtrapolationSeparate observable.
 """
 
 
@@ -451,7 +452,7 @@ class ExponentialExtrapolation(mfdnres.observable.Observable):
         observable_label_delimiters = self._observable_label_delimiters
         if observable_label_delimiters is None:
             observable_label_delimiters = ("[","]")
-        return r"{}{}{} \mathrm{{exp-3}}".format(
+        return r"{}{}{} \mathrm{{exp3}}".format(
             observable_label_delimiters[0],
             observable_label_text,
             observable_label_delimiters[1],
@@ -463,3 +464,111 @@ class ExponentialExtrapolation(mfdnres.observable.Observable):
         """
         axis_label_text = self._argument.axis_label_text
         return axis_label_text
+
+
+################################################################
+# deduced observable: ExcitationEnergyExponentialExtrapolationSeparate
+################################################################
+
+class ExcitationEnergyExponentialExtrapolationSeparate(mfdnres.observable.Observable):
+    """Observable extractor for 3-point eponential extrapolation of excitation
+    energy, done as difference of independently extrapolated absolute energies.
+
+    Results are equivalent to, but labeling different from, those of
+
+        mfdnres.observable.Difference(
+            mfdnres.observable_differencing.ExponentialExtrapolation(
+                mfdnres.observable.Energy(
+                    nuclide,
+                    level,  # e.g., mfdnres.level.LevelQN(qn_ex),
+                )
+            ),
+            mfdnres.observable_differencing.ExponentialExtrapolation(
+                mfdnres.observable.Energy(
+                    nuclide,
+                    reference_level,  # e.g., mfdnres.level.LevelQN(qn),
+                )
+            ),
+        ),
+
+    """
+
+    def __init__(self,  nuclide, level, reference_level, *, Nmax_shift=0, label_as_difference=False):
+        """ Initialize with given parameters.
+
+        Arguments:
+
+            nuclide (tuple): (Z, N)
+
+            level (LevelSelector): level
+
+            reference_level (LevelSelector): reference ("ground state") level for energy difference
+
+            Nmax_shift (int, optional): shift in Nmax to apply to excited level,
+            for cross-parity excitation energies (e.g., Nmax_shift=1 to
+            calculated "Nmax+1" excited level energy relative to "Nmax"
+            reference level energy, reported at "Nmax")
+
+            label_as_difference (bool, optional): whether or not to show reference level in observable label
+
+        """
+        super().__init__()
+        self._nuclide = nuclide
+        self._level = level
+        self._reference_level = reference_level
+        self._Nmax_shift = Nmax_shift
+        self._label_as_difference = label_as_difference
+
+    def data(self, mesh_data, key_descriptor, verbose=False):
+        """ Extract data frame of observable values over mesh.
+        """
+        level_energy_value_mesh = mfdnres.observable.Energy(self._nuclide, self._level).data(mesh_data, key_descriptor, verbose=verbose)
+        level_energy_value_mesh = mfdnres.observable.Nmax_shifted(level_energy_value_mesh, self._Nmax_shift)
+        reference_level_energy_value_mesh = mfdnres.observable.Energy(self._nuclide, self._reference_level).data(mesh_data, key_descriptor, verbose=verbose)
+        return Nmax_extrapolation_exp3(level_energy_value_mesh) - Nmax_extrapolation_exp3(reference_level_energy_value_mesh)
+
+    @property
+    def descriptor_str(self):
+        """ Text string describing observable.
+        """
+        arithmetic_operation = "extrapolation-exp3-sep"  # flag that extrapolation is separate for each level's energy
+
+        descriptor_str = "-".join([
+            mfdnres.data.nuclide_str(self._nuclide),
+            "energy-ex",
+            self._level.descriptor_str,
+            self._reference_level.descriptor_str,
+        ])
+
+        return r"{}_{}".format(
+            arithmetic_operation,
+            descriptor_str,
+        )
+
+    @property
+    def observable_label_text(self):
+        """ Formatted LaTeX text representing observable.
+        """
+        level_text = self._level.label_text
+        reference_level_text = self._reference_level.label_text
+        if self._label_as_difference:
+            observable_text = r"E"
+            label = r"{}({})-{}({})".format(observable_text,level_text,observable_text,reference_level_text)
+        else:
+            observable_text = r"E_x"
+            label = r"{}({})".format(observable_text,level_text)
+        observable_text = "{} \mathrm{{exp3'}".format(observable_text)
+        return label
+
+    @property
+    def axis_label_text(self):
+        """ Formatted LaTeX text representing axis label.
+        """
+        if self._label_as_difference:
+            observable_text = r"\Delta E"
+        else:
+            observable_text = r"E_x"
+        units_text = r"\mathrm{MeV}"
+        return observable_text, units_text
+
+    
